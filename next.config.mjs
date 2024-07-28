9f910c6003e40d4956ea5466469593ed44e5f75a
veritasnz <7684225+veritasnz@users.noreply.github.com>
2024-07-28 14:58:49 +0900

    chore: switch to next-translate-plugin, add configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
 
-export default nextConfig;
+import nextTranslate from "next-translate-plugin";
+import withPlaiceholder from "@plaiceholder/next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default withPlaiceholder(
+  bundleAnalyzer(
+    nextTranslate({
+      reactStrictMode: true,
+      i18n: {
+        locales: ["en", "jp"],
+        defaultLocale: "en",
+      },
+      webpack(config) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                // HACK: Stop SVGR from prefixing classnames inside of the SVGs
+                svgoConfig: { plugins: [] },
+              },
+            },
+          ],
+        });
+
+        return config;
+      },
+    })
+  )
+);
