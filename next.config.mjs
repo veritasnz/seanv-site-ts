06d978aa1d59cb961376f5bc12d8e05a640d8600
veritasnz <7684225+veritasnz@users.noreply.github.com>
2024-07-28 16:17:52 +0900

    refac: remove mention of plaiceholder

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,34 @@
 /** @type {import('next').NextConfig} */
 
-import nextTranslate from "next-translate-plugin";
-import withPlaiceholder from "@plaiceholder/next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
+import nextTranslate from "next-translate-plugin";
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
 });
 
-export default withPlaiceholder(
-  bundleAnalyzer(
-    nextTranslate({
-      reactStrictMode: true,
-      i18n: {
-        locales: ["en", "jp"],
-        defaultLocale: "en",
-      },
-      webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/,
-          use: [
-            {
-              loader: "@svgr/webpack",
-              options: {
-                // HACK: Stop SVGR from prefixing classnames inside of the SVGs
-                svgoConfig: { plugins: [] },
-              },
+export default bundleAnalyzer(
+  nextTranslate({
+    reactStrictMode: true,
+    i18n: {
+      locales: ["en", "jp"],
+      defaultLocale: "en",
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              // HACK: Stop SVGR from prefixing classnames inside of the SVGs
+              svgoConfig: { plugins: [] },
             },
-          ],
-        });
+          },
+        ],
+      });
 
-        return config;
-      },
-    })
-  )
+      return config;
+    },
+  })
 );
